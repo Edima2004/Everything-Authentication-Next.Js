925f2da3525f7b17573ff886972b2d0242333660
Edima Vincent-Thompsom <edimavthompson@gmail.com>
2024-07-19 23:51:34 +0100

    email authentication cont'd

--- actions/new-verification.ts
+++ actions/new-verification.ts
@@ -2,7 +2,10 @@
 
 import { db } from '@/lib/db';
 import { getUserByEmail } from '@/data/user';
-import { getVerificationTokenByToken } from '@/data/verification-token';
+import {
+	getVerificationTokenByEmail,
+	getVerificationTokenByToken,
+} from '@/data/verification-token';
 
 export const newVerification = async (token: string) => {
 	const existingToken = await getVerificationTokenByToken(token);
@@ -14,5 +17,28 @@ export const newVerification = async (token: string) => {
 	const hasExpired = new Date(existingToken.expires) < new Date();
 
 	if (hasExpired) {
+		return { error: 'Token has expired!' };
 	}
+
+	const existingUser = await getUserByEmail(existingToken.email);
+	if (!existingUser) {
+		return { error: 'Email does not exist!' };
+	}
+
+	await db.user.update({
+		where: {
+			id: existingUser.id,
+		},
+		data: {
+			emailVerified: new Date(),
+			email: existingToken.email,
+		},
+	});
+
+	await db.verificationToken.delete({
+		where: {
+			id: existingToken.id,
+		},
+	});
+	return { success: 'Email verified!' };
 };
--- components/auth/new-verification-form.tsx
+++ components/auth/new-verification-form.tsx
@@ -1,31 +1,51 @@
 "use client"
+import { newVerification } from "@/actions/new-verification"
 import CardWrapper from "@/components/auth/card-wrapper"
 import { useSearchParams } from "next/navigation"
-import { useCallback, useEffect } from "react"
+import { useCallback, useEffect, useState } from "react"
 import { BeatLoader} from 'react-spinners'
+import { FormError } from "../form-error"
+import { FormSuccess } from "../form-success"
 const NewVerificationForm = ()=>{
    const searchParams = useSearchParams();
-
+   const [error, setError] = useState <string | undefined>()
+   const [success, setSuccess] = useState <string | undefined>()
    const token = searchParams.get("token");
 
    const onSubmit = useCallback(()=>{
-      console.log(token);
-   }, [token]);
+      if(success || error) return;
+      if(!token){
+         setError("Missing Token!")
+         return;
+      };
+      //console.log(token);
+      newVerification(token || '')
+      .then((data) => {
+         setSuccess(data.success);
+         setError(data.error);
+      })
+      .catch(()=>{
+         setError("Something went wrong!")
+      })
+   }, [token, success, error]);
 
    useEffect(() => {
       onSubmit();
    }, [onSubmit])
    
-   return(
-      <CardWrapper
-      headerLabel="Confirming your verification"
-      backButtonHref="/auth/login"
-      backButtonLabel="Back to login">
-         <div className="flex items-center w-full justify-center">
-         <BeatLoader/>
-         </div>
-      </CardWrapper>
-   )
+   return (
+			<CardWrapper
+				headerLabel="Confirming your verification"
+				backButtonHref="/auth/login"
+				backButtonLabel="Back to login"
+			>
+				<div className="flex items-center w-full justify-center">
+					{!success && !error && <BeatLoader />}
+					{!success && (<FormError message={error} />)}
+					<FormSuccess message={success} />
+				</div>
+			</CardWrapper>
+		);
 }
 
 export default NewVerificationForm
